e015e997981f7d61c7bf8f517a76b93d565c5ed0
WongJk <jk.wong@qq.com>
2016-12-07 20:22:37 +0800

    完成calc_core的计算核心算法，支持加减乘除，其余方法可以在此基础上做状态扩展

--- src/calc_core.jsx
+++ src/calc_core.jsx
@@ -29,24 +29,25 @@ class CalcCore {
      * @param  {string|number} value 初始化原始值
      */
     constructor (value) {
+        // 计算核心对外输出的值，可能是临时计算值，也可能是最后的计算结果
         this.value = value || 0;
 
         // 加减运算符
         this.addsubOpers = new Set([
             '+', '-'
-        ]);;
+        ]);
         // 乘除运算符
         this.muldivOpers = new Set([
             '*', '/'
-        ]);;
+        ]);
         // 其他
         this.otherOpers = new Set([
             'AC', '+/-', '%', '=', '.'
-        ]);;
+        ]);
         // 数字
         this.numberOpers = new Set([
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-        ]);;
+        ]);
 
         /**
          * 计算相关的操作数
@@ -63,31 +64,68 @@ class CalcCore {
     }
 
     initStateMachine () {
+        let core = this;
 
-        // 进入stageN时的回调 
-        let onstage0 = function (event, from, to, command) {
-
+        // 进入stageN时的回调
+        let onenterstage0 = function (event, from, to, command, optMsg = {}) {
+            // 初始化 initial 时，command是undefined
+            command = command || 0;
+            core.a = core.refreshNumber(core.a, command);
+            core.value = core.a;
         };
-        let onstage1 = function (event, from, to, command) {
+        // 离开stageN时的回调
+        let onleavestage0 = function (event, from, to, command, optMsg = {}) {};
 
+        let onenterstage1 = function (event, from, to, command, optMsg = {}) {
+            core.op1 = command;
         };
-        let onstage2 = function (event, from, to, command) {
+        let onleavestage1 = function (event, from, to, command, optMsg = {}) {};
 
+        let onenterstage2 = function (event, from, to, command, optMsg = {}) {
+            core.op1 = command;
         };
-        let onstage3 = function (event, from, to, command) {
+        let onleavestage2 = function (event, from, to, command, optMsg = {}) {};
 
+        let onenterstage3 = function (event, from, to, command, optMsg = {}) {
+            core.b = core.refreshNumber(core.b, command);
+            core.value = core.b;
         };
-        let onstage4 = function (event, from, to, command) {
+        let onleavestage3 = function (event, from, to, command, optMsg = {}) {};
 
+        let onenterstage4 = function (event, from, to, command, optMsg = {}) {
+            core.op2 = command;
         };
-        let onstage5 = function (event, from, to, command) {
+        let onleavestage4 = function (event, from, to, command, optMsg = {}) {};
 
+        let onenterstage5 = function (event, from, to, command, optMsg = {}) {
+            core.c = core.refreshNumber(core.c, command);
+            core.value = core.c;
         };
-        let onstage6 = function (event, from, to, command) {
-
+        let onleavestage5 = function (event, from, to, command, optMsg = {}) {
+            core.b = core.evaluate(core.b, core.op2, core.c);
+            core.c = null;
         };
-        let onstage7 = function (event, from, to, command) {
 
+        let onenterstage6 = function (event, from, to, command, optMsg = {}) {
+            core.b = core.refreshNumber(core.b, command);
+        };
+        let onleavestage6 = function (event, from, to, command, optMsg = {}) {
+            core.a = core.evaluate(core.a, core.op1, core.b);
+            core.b = null;
+        };
+
+        let onenterstage7 = function (event, from, to, command, optMsg = {}) {
+            core.op2 = command;
+            core.c = null;
+            core.value = core.evaluate(core.a, core.op1, core.b);
+        };
+        let onleavestage7 = function (event, from, to, command, optMsg = {}) {
+            if (optMsg.commandType === 'number') {
+                core.a = core.evaluate(core.a, core.op1, core.b);
+                core.op1 = core.op2;
+                core.b = null;
+                core.op2 = null;
+            }
         };
 
         return stateMachine.create({
@@ -135,24 +173,85 @@ class CalcCore {
                 /* end stage7 */
             ],
             callbacks: {
-                onbeforeevent: function (event, from, to, command) {
-                    switch (this.current) {
-                        case 'stage0':
-                        case 'stage1':
-                        case 'stage2':
-                        case 'stage3':
-                        case 'stage4':
-                        case 'stage5':
-                        case 'stage6':
-                        case 'stage7':
-                            break;
-                        
-                        default: 
-                            // ..
+                /**
+                 * 当状态不变时，比如：stage0时输入的是number
+                 * 不会触发onleavestate & onenterstate事件
+                 * 所以在这里统一处理 自循环状态转换，主动触发onenterstate事件的回调
+                 */
+                onbeforeevent: function (event, from, to, command, optMsg = {}) {
+                    let commandType = optMsg.commandType;
+
+                    if (this.is('stage0') && commandType === 'number') {
+                        onenterstage0(event, from, to, command, optMsg);
                     }
+                    else if (this.is('stage1') && commandType === 'addsub') {
+                        onenterstage1(event, from, to, command, optMsg);
+                    }
+                    else if (this.is('stage2') && commandType === 'muldiv') {
+                        onenterstage2(event, from, to, command, optMsg);
+                    }
+                    else if (this.is('stage3') && commandType === 'number') {
+                        onenterstage3(event, from, to, command, optMsg);
+                    }
+                    else if (this.is('stage4') && commandType === 'muldiv') {
+                        onenterstage4(event, from, to, command, optMsg);
+                    }
+                    else if (this.is('stage5') && commandType === 'number') {
+                        onenterstage5(event, from, to, command, optMsg);
+                    }
+                    else if (this.is('stage6') && commandType === 'number') {
+                        onenterstage6(event, from, to, command, optMsg);
+                    }
+                    else if (this.is('stage7') && commandType === 'addsub') {
+                        onenterstage7(event, from, to, command, optMsg);
+                    }
+                    else {
+                        // ignore
+                    }
+                },
+                onenterstage0, onleavestage0, onenterstage1, onleavestage1,
+                onenterstage2, onleavestage2, onenterstage3, onleavestage3,
+                onenterstage4, onenterstage4, onenterstage5, onleavestage5,
+                onenterstage6, onleavestage6, onenterstage7, onleavestage7,
+                onafterevent: function (...args) {
+                    // debug信息
+                    console.log(...args);
+                    console.log(core.a, core.op1, core.b, core.op2, core.c);
                 }
             }
         });
+    }
+
+    /**
+     * 求值函数，传入表达式，利用eval计算得值
+     * @param  {...command} args    数字or运算符
+     * @return {number}             计算后的值
+     */
+    evaluate(...args) {
+        let value = null;
+        try {
+            value = eval(args.join(''));
+        }
+        catch (e) {
+            alert('boom!~');
+        }
+        return value;
+    }
+
+    /**
+     * 接受一个旧数字，一个新数字（单个字符），生成一个更新后的数字
+     * 比如：oldNumber是12，newNumber是3，则输出123
+     * @param  {number} oldNumber 旧数字
+     * @param  {number} newNumber 新数字
+     * @return {number}           结果
+     */
+    refreshNumber(oldNumber, newNumber) {
+        if (oldNumber === null) {
+            return parseInt(newNumber, 10);
+        }
+        oldNumber = parseInt(oldNumber, 10);
+        newNumber = parseInt(newNumber, 10);
+        return oldNumber * 10 + newNumber;
     }
 
     /**
@@ -161,22 +260,38 @@ class CalcCore {
      */
     compute(command) {
         if (this.addsubOpers.has(command)) {
-            this.sm.addsub(command);
+            this.sm.addsub(command, {
+                commandType: 'addsub'
+            });
         }
         else if (this.muldivOpers.has(command)) {
-            this.sm.muldiv(command);
+            this.sm.muldiv(command, {
+                commandType: 'muldiv'
+            });
         }
         else if (this.numberOpers.has(command)) {
-            this.sm.number(command);
+            this.sm.number(command, {
+                commandType: 'number'
+            });
         }
         else if (command === '=') {
-            console.log(command);
+            let args = [
+                this.a, this.op1, this.b, this.op2, this.c
+            ].map(x => x === null ? '' : x);
+            console.log(...args);
+            this.a = this.evaluate(...args);
+            this.value = this.a;
+
+            // 重置除了a之外的变量
+            [
+                this.a, this.op1, this.b, this.op2, this.c
+            ] = [
+                this.a, null, null, null, null
+            ];
         }
         else {
             console.log(`not support ${command} yet.`);
         }
-
-        console.log(command, this.sm.current);
     }
 
     /**
